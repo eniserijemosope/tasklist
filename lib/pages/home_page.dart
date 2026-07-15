03ae347f042e87bc1a4209ea6f1bb97cad6e692b
Mosopefoluwa <eniserijemosope@gmail.com>
2026-07-15 14:52:48 +0100

    bottom bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,15 +3,33 @@ import 'package:tasklist/widget/custom_app_bar.dart';
 import 'package:tasklist/widget/nav_bottom_bar.dart';
 import 'package:tasklist/widget/nav_drawer.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; // Tracks active screen
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
-      drawer: NavDrawer(),
-      bottomNavigationBar: NavBottomBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      drawer: const NavDrawer(),
+      bottomNavigationBar: CustomBottomNav(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index; // Rebuilds the UI with the selected page
+          });
+        },
+      ),
+      body: Center(
+        child: Text('Active Screen Index: $_selectedIndex'), // Swap this with actual pages later
+      ),
     );
   }
 }
--- lib/widget/nav_bottom_bar.dart
+++ lib/widget/nav_bottom_bar.dart
@@ -1,47 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
+import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 
-class NavBottomBar extends StatelessWidget {
-  const NavBottomBar({super.key});
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNav({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-      decoration: BoxDecoration(
-        color: Colors.green[800],
-        borderRadius: BorderRadius.circular(30),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.15),
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white70,
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.water_drop_outlined),
-              label: '',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications_none_rounded),
-              label: '',
-            ),
-          ],
+    return CurvedNavigationBar(
+      index: currentIndex,
+      backgroundColor: const Color(0xFFB7D7B0),
+      color: const Color(0xFFB7D7B0),
+      buttonBackgroundColor: const Color(0xFF7FAF7B),
+      items: [
+        CurvedNavigationBarItem(
+          child: const Icon(Icons.person_outline, color: Colors.white),
+          label: 'Profile',
         ),
-      ),
+        CurvedNavigationBarItem(
+          child: const Icon(Icons.water_drop_outlined, color: Colors.white),
+          label: 'Water',
+        ),
+        CurvedNavigationBarItem(
+          child: const Icon(Icons.notifications_none_rounded, color: Colors.white),
+          label: 'Notification',
+        ),
+      ],
+      onTap: onTap,
     );
   }
 }
